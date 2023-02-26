2f80a6072f2625cdb6127520b5a14c1fe3744df7
farrasarira <mfarrasa01@gmail.com>
2023-02-27 00:27:14 +0700

    add input parameters in cylinder_generator

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -1,8 +1,11 @@
-#include "./headers/setup.hpp"
 #include "./headers/lbm.hpp"
+#include <omp.h>
 
-void cylinder_generator(LBM &lb)
+void cylinder_generator(LBM &lb, int D)
 {
+    int Nx = lb.getNx();
+    int Ny = lb.getNy();
+    int Nz = lb.getNz();
     double rad=D/2;         // radius of cylinder
     double cx=dx*(Nx/5);    // x-center of cylinder
     double cy=dx*(Ny/2);    // y-center of cylinder
@@ -10,10 +13,13 @@ void cylinder_generator(LBM &lb)
     #pragma omp parallel for
     for(int i=0; i<Nx; i++){
         for(int j=0; j<Ny; j++){
-            double rx=dx*i-dx*0.5;
-            double ry=dx*j-dx*0.5;
-            if(sqrt(pow(cx-rx,2)+pow(cy-ry,2))<=rad){
-                lb.fluid1[i][j].type=TYPE_S;
+            for(int k=0; k<Nz; ++k)
+            {
+                double rx=dx*i-dx*0.5;
+                double ry=dx*j-dx*0.5;
+                if(sqrt(pow(cx-rx,2)+pow(cy-ry,2))<=rad){
+                    lb.fluid1[i][j][k].type=TYPE_S;
+                }
             }
         }
     }
