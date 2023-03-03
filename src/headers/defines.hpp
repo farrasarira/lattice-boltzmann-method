41394b8b5af84fd158bf6b0bbb29a79ebd73106a
farrasarira <mfarrasa01@gmail.com>
2023-03-03 16:12:11 +0700

    adding 3D Taylor Green Option

--- src/headers/defines.hpp
+++ src/headers/defines.hpp
@@ -3,39 +3,75 @@
 
     #include<math.h>
 
+    //_______________________________________________________________________________________________________
+    // Input and choose the simulation condition
 
-    // ################ Lattice Size ##################
-    #define dx 1
-    #define dy 1
-    #define dt 1
+    // ################ Flow Cases ####################
+    // Choose one of the following Flow Cases
+    //#define CYLINDER_2D
+    #define TAYLOR_GREEN_3D
+    //#define TAYLOR_GREEN_2D
+
+
+    // ############# Flow Parameters ##################
+    // input the flow parameters
+    #define RE 200.0   // Reynolds number
+    #define NU 0.05     // Kinetic viscosity
+    #define TEMP 0.4    // Temperature
     #define rho0 1.0            // Density
     #define T0 1.0              // Temperature high
+    #define P0 0.0
     #define cs 1.0/sqrt(3.0)    // Lattice sound speed
+    #define GAS_CONST 0.8
+
+    // ########## Simulation Time & Output ############
+    #define NSTEP 10000   // Maximum time step, in Lattice time unit
+    #define TOUT 100        // Interval of time step to save the macroscopic quantity
+
+    // ############# Physical Quantity ################
+    #define BCXM  0.0   
+    #define BCXP  0.001
+    #define BCYM  0.0
+    #define BCYP  0.001
+
+    // ############# Lattice Parameters ###############
+    #define dx 1
+    #define dy 1
+    #define dz 1
+    #define dt 1
+    #define NX 100
+    #define NY 100
+    #define NZ 100
+       
 
     // ################# LBM Model ####################
-    // #define LBM_ENTROPY      // LBM model based on entropy
-     #define LBM_CONV            // Conventional LBM
+    // Choose one of the following LBM Model
+    #define LBM_ENTROPY      // LBM model based on entropy
+    //#define LBM_CONV            // Conventional LBM
 
-    // ################ Velocity Set ##################
-    //#define D1Q3 // choose D1Q3 velocity set for 1D;
-    #define D2Q9 // choose D2Q9 velocity set for 2D; 
-    //#define D3Q15 // choose D3Q15 velocity set for 3D;
-    //#define D3Q19 // choose D3Q19 velocity set for 3D; 
-    //#define D3Q27 // choose D3Q27 velocity set for 3D;
 
-    // ######### Single/Multicomponent flow ###########
-    #define SCF // choose singlecomponent flow
-    //#define MCF // choose multicomponent flow
+
+
+
+
+    // _____________________________________________________________________________________________________
+    // Definition For code, don't change!
+
+    // ################# Velocity Set ##################
+    #if defined CYLINDER_2D || defined TAYLOR_GREEN_2D
+        #define D2Q9
+        #define NDIM 2
+    #elif defined TAYLOR_GREEN_3D
+        #define D3Q27
+        #define NDIM 3
+    #endif
 
     // ############## Boundary Condition ###############
     #define TYPE_F 0 // fluid domain
     #define TYPE_S 1 // (stationary or moving) solid boundary
     #define TYPE_E 2 // equilibrium boundary (inflow/outflow)
-    #define TYPE_T 3 // temperature boundary
-    #define TYPE_P 4 // periodic boundary condition
+    #define TYPE_P 3 // periodic boundary
+    #define TYPE_T 4 // temperature boundary
 
-    // Parameter
-    #define GAS_CONST 1
-    #define TEMP 0.4
 
 #endif
