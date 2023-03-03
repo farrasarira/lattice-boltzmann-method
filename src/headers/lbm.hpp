afa49eb5d7c3632c2fedbe129f30602f83369135
farrasarira <mfarrasa01@gmail.com>
2023-03-03 16:13:01 +0700

    add D3Q27 LBM

--- src/headers/lbm.hpp
+++ src/headers/lbm.hpp
@@ -10,26 +10,29 @@
         // D2Q9 velocity sets
         const int ndim = 2;
         const int npop = 9;
-        const double cx[9] = {0.0,+1.0, 0.0,-1.0, 0.0,+1.0,-1.0,-1.0,+1.0};
-        const double cy[9] = {0.0, 0.0,+1.0, 0.0,-1.0,+1.0,+1.0,-1.0,-1.0};
+        const double cx[9] = {0.0,+1.0,-1.0, 0.0, 0.0,+1.0,-1.0,+1.0,-1.0};
+        const double cy[9] = {0.0, 0.0, 0.0,+1.0,-1.0,+1.0,-1.0,-1.0,+1.0};
         const double cz[9] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
         // D2Q9 weight factor | cs^2 = 1/3
-        const double w[9] = {4./9,1./9,1./9,1./9,1./9,1./36,1./36,1./36,1./36};
+        const double wi[9]  = {4./9,1./9,1./9,1./9,1./9,1./36,1./36,1./36,1./36};
+                                                                                                                 
     #elif defined D3Q15
         const int ndim = 3;
         const int npop = 15;
         const double cx[15] = {0.0,+1.0,-1.0, 0.0, 0.0, 0.0, 0.0,+1.0,+1.0,+1.0,+1.0,-1.0,-1.0,-1.0,-1.0};
         const double cy[15] = {0.0, 0.0, 0.0,+1.0,-1.0, 0.0, 0.0,+1.0,+1.0,-1.0,-1.0,+1.0,+1.0,-1.0,-1.0};
         const double cz[15] = {0.0, 0.0, 0.0, 0.0, 0.0,+1.0,-1.0,+1.0,-1.0,+1.0,-1.0,+1.0,-1.0,+1.0,-1.0};
-        const double w[15] =  {2/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9,1/72,1/72,1/72,1/72,1/72,1/72,1/72,1/72};
+        const double wi[15] =  {2/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9,1/72,1/72,1/72,1/72,1/72,1/72,1/72,1/72};
     #elif defined D3Q27
         const int ndim = 3;
         const int npop = 27;
-        const double cx[27] = { 0.0,+1.0,-1.0, 0.0, 0.0, 0.0, 0.0,+1.0,+1.0,-1.0,-1.0,+1.0,+1.0,-1.0,-1.0, 0.0, 0.0, 0.0, 0.0, +1.0, +1.0, +1.0, +1.0, -1.0, -1.0, -1.0, -1.0};
-        const double cy[27] = { 0.0, 0.0, 0.0,+1.0,-1.0, 0.0, 0.0,+1.0,-1.0,+1.0,-1.0, 0.0, 0.0, 0.0, 0.0,+1.0,-1.0,+1.0,-1.0, +1.0, -1.0, +1.0, -1.0, +1.0, -1.0, +1.0, -1.0};
-        const double cz[27] = { 0.0, 0.0, 0.0, 0.0, 0.0,+1.0,-1.0, 0.0, 0.0, 0.0, 0.0,+1.0,-1.0,+1.0,-1.0,+1.0,-1.0,-1.0,+1.0, +1.0, -1.0, -1.0, +1.0, +1.0, -1.0, -1.0, +1.0};
-        const double w[27] =  {2/27,2/27,2/27,2/27,2/27,2/27,2/27,1/54,1/54,1/54,1/54,1/54,1/54,1/54,1/54,1/54,1/54,1/54,1/54,1/216,1/216,1/216,1/216,1/216,1/216,1/216,1/216};
+        const double cx[27] = { 0.0,+1.0,-1.0, 0.0, 0.0,+1.0,-1.0,+1.0,-1.0, 0.0, 0.0,+1.0,-1.0,+1.0,-1.0, 0.0, 0.0, 0.0, 0.0, +1.0,-1.0,+1.0,-1.0,+1.0,-1.0,+1.0,-1.0};
+        const double cy[27] = { 0.0, 0.0, 0.0,+1.0,-1.0,+1.0,-1.0,-1.0,+1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,+1.0,-1.0,+1.0,-1.0, +1.0,-1.0,-1.0,+1.0,+1.0,-1.0,-1.0,+1.0};
+        const double cz[27] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,+1.0,-1.0,+1.0,-1.0,-1.0,+1.0,+1.0,-1.0,-1.0,+1.0, +1.0,-1.0,-1.0,+1.0,-1.0,+1.0,+1.0,-1.0};
+        const double wi[27] = {8./27,2./27,2./27,2./27,2./27,1./54,1./54,1./54,1./54,2./27,2./27,1./54,1./54,1./54,1./54,1./54,1./54,1./54,1./54,1./216,1./216,1./216,1./216,1./216,1./216,1./216,1./216};
     #endif
+
+    const int opposite[27] = {0,2,1,4,3,6,5,8,7,10,9,12,11,14,13,16,15,18,17,20,19,22,21,24,23,26,25};
 
     class LATTICE
     {
@@ -38,16 +41,16 @@
             
             // ###### Momentum Kinetic Equation Parameter ######
             double f[npop], fpc[npop];    // distribution function, distribution function post collistion  
-            double rho;     // macroscopic quantity
-            double u;       // velocity in x-direction
-            double v;       // velocity in y-direction
-            double w = 0;   // velocity in z-direction
+            double rho;           // macroscopic quantity
+            double u = 0.0;       // velocity in x-direction
+            double v = 0.0;       // velocity in y-direction
+            double w = 0.0;       // velocity in z-direction
 
-            // ####### Energy Kinetic Equation Parameter #######
+            /* // ####### Energy Kinetic Equation Parameter #######
             double g[npop], gpc[npop];    // energy distribution function, energy distrbution function post collision
             double temp = 0.8;      // temperature
             double totalEnergy;     // total energy (E)
-            double internalEnergy;  // internal energy (U) | E = U + 1/2 * rho * u^2 
+            double internalEnergy;  // internal energy (U) | E = U + 1/2 * rho * u^2 */
 
     };
 
@@ -77,6 +80,7 @@
             int getNx(){return Nx;};
             int getNy(){return Ny;};
             int getNz(){return Nz;};
+            double getNu(){return nu;};
     };
 
 #endif
--- src/headers/setup.hpp
+++ src/headers/setup.hpp
@@ -3,28 +3,42 @@
 
     #include "lbm.hpp"
     #include "geometry.hpp"
-    #include<iostream>
+    #include <iostream>
+    #include <math.h>
+    #include <omp.h>
 
+    #if defined CYLINDER_2D
     LBM main_setup() // 2D Flow over cylinder --------------------------------------------------------
     {
-        LBM lb(1000,500,1,0.002);
+        LBM lb(4000,4000,1,NU);
         int Nx = lb.getNx(); int Ny = lb.getNy(); int Nz = lb.getNz();
 
         int D = Ny/10; 
-        double nu = 0.002;
-        double Re = 1000;
-        double u_max = Re * nu / D;
-        double Ma = u_max * cs;
+        double nu = lb.getNu();
+        double u_max = RE * nu / D;
 
         cylinder_generator(lb, D);
 
+        #pragma omp parallel for
         for(int i = 0; i < Nx ; ++i)
         {
             for(int j = 0; j < Ny; ++j)
             {
                 for(int k = 0; k < Nz; ++k)
                 {
-                    if (lb.fluid1[i][j][k].type == TYPE_F)
+                    if (j == 0 || j == Ny-1) 
+                    {
+                        lb.fluid1[i][j][k].type = TYPE_S;  // Solid Boundary in upper and lower side
+                    }
+                    if (i == 0 || i == Nx-1)
+                    {
+                        lb.fluid1[i][j][k].type = TYPE_E;  // Equilibrium inlet and outlet
+                    }
+                    if (k == 0 || k == Nz-1)
+                    {
+                        lb.fluid1[i][j][k].type = TYPE_P;
+                    }
+                    if (lb.fluid1[i][j][k].type == TYPE_F || lb.fluid1[i][j][k].type == TYPE_E)  // Fluid Domain
                     {
                         lb.fluid1[i][j][k].rho = 1.0;
                         lb.fluid1[i][j][k].u = u_max;
@@ -37,6 +51,84 @@
 
         return lb;
     }
-    
+
+    #elif defined TAYLOR_GREEN_3D
+    LBM main_setup() // 3D Taylor-Green Vortex
+    {
+        LBM lb(NX,NY,NZ,NU);
+        int Nx = lb.getNx(); int Ny = lb.getNy(); int Nz = lb.getNz();
+        double nu = lb.getNu();
+
+        double Re = RE;  // set Reynolds number
+        const double u_max = Re * nu / Nx;    // maximum initial velocity
+        const unsigned int periodicity = 1;
+        const double a = (double)Nx/periodicity;
+        const double b = (double)Ny/periodicity;
+        const double c = (double)Nz/periodicity;
+        
+        #pragma omp parallel for
+        for(int i = 0; i < Nx ; ++i)
+        {
+            for(int j = 0; j < Ny; ++j)
+            {
+                for(int k = 0; k < Nz; ++k)
+                {
+                    if (i==0 || i==Nx-1 ||  j==0 || j==Ny-1 || k==0 || k==Nz-1) // set periodic boundary condition
+                    {
+                        lb.fluid1[i][j][k].type = TYPE_P;
+                    }
+                    if (lb.fluid1[i][j][k].type == TYPE_F)
+                    {
+                        lb.fluid1[i][j][k].u = u_max * sin(2.0*M_PI/a*(double)i) * cos(2.0*M_PI/b*(double)j) * sin(2.0*M_PI/c*(double)k);
+                        lb.fluid1[i][j][k].v = -u_max * cos(2.0*M_PI/a*(double)i) * sin(2.0*M_PI/b*(double)j) * sin(2.0*M_PI/c*(double)k);;
+                        lb.fluid1[i][j][k].w = 0;
+                        lb.fluid1[i][j][k].rho = 1.0 - u_max*u_max * 3.0/4.0 * (cos(4.0*M_PI/a*(double)i) + cos(4.0*M_PI/b*(double)j));
+                        
+                    }
+                }
+            }
+        }
+        return lb;
+    }
+    #elif defined TAYLOR_GREEN_2D
+    LBM main_setup() // 2D Taylor-Green Vortex
+    {
+        LBM lb(100,100,100,NU);
+        int Nx = lb.getNx(); int Ny = lb.getNy(); int Nz = lb.getNz();
+        double nu = lb.getNu();
+
+        double Re = RE;  // set Reynolds number
+        const double u_max = Re * nu / Nx;    // maximum initial velocity
+        const unsigned int periodicity = 1;
+        const double a = (double)Nx/periodicity;
+        const double b = (double)Ny/periodicity;
+        
+        #pragma omp parallel for
+        for(int i = 0; i < Nx ; ++i)
+        {
+            for(int j = 0; j < Ny; ++j)
+            {
+                for(int k = 0; k < Nz; ++k)
+                {
+                    if (i==0 || i==Nx-1 ||  j==0 || j==Ny-1) // set periodic boundary condition
+                    {
+                        lb.fluid1[i][j][k].type = TYPE_P;
+                    }
+                    if (lb.fluid1[i][j][k].type == TYPE_F)
+                    {
+                        double X = i + 0.5;
+                        double Y = j + 0.5;
+                        lb.fluid1[i][j][k].u = -u_max * cos(2.0*M_PI/a*X) * sin(2.0*M_PI/b*Y) ;
+                        lb.fluid1[i][j][k].v =  u_max * sin(2.0*M_PI/a*X) * cos(2.0*M_PI/b*Y) ;
+                        lb.fluid1[i][j][k].rho = 1.0 - u_max*u_max * 3.0/4.0 * (cos(4.0*M_PI/a*X) + cos(4.0*M_PI/b*Y));
+                        
+                    }
+                }
+            }
+        }
+        return lb;
+    }
+    #endif
+   
 #endif
 
